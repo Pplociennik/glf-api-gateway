721b4d37daf72bebe5d622a9b251f1c49f2ba897
pplociennik <pplociennik097@gmail.com>
2024-09-17 19:03:51 +0200

    [NO_JIRA] DefaultFallbackController: added javadoc and modified the return message.

--- src/main/java/com/goaleaf/gateway/controller/DefaultFallbackController.java
+++ src/main/java/com/goaleaf/gateway/controller/DefaultFallbackController.java
@@ -5,9 +5,14 @@ import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
 
 /**
- * TODO: Describe this class.
- *
- * @author Created by: Pplociennik at 17.09.2024 17:41
+ * Controller for handling fallback routes in the Goaleaf application.
+ * <p>
+ * This controller provides endpoints that are used as fallback mechanisms
+ * when a service is unavailable or an error occurs. The fallback routes
+ * return predefined responses to the client.
+ * <p>
+ * Author: Pplociennik
+ * Date: 17.09.2024
  */
 @RestController
 @RequestMapping( path = "/fallback" )
@@ -15,6 +20,6 @@ public class DefaultFallbackController {
 
     @RequestMapping( "/contactSupport" )
     Mono< String > contactSupport() {
-        return Mono.just( "An error occurred. Please contact the support team." );
+        return Mono.just( "An error occurred. Please try again later or contact the support team." );
     }
 }
